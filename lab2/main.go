89f6d2af91d1ddd7908f51f40d5041e69df9fc57
Marius <mariukas.ambrazevicius@gmail.com>
2024-11-04 18:23:33 +0200

    Minor fix.

--- lab2/main.go
+++ lab2/main.go
@@ -3,9 +3,7 @@ package main
 import (
 	"fmt"
 
-	"marius.org/cat"
 	jsonreader "marius.org/json_reader"
-	"marius.org/requests"
 )
 
 const FILE_NAME string = "cats.json"
@@ -16,11 +14,5 @@ func main() {
 	for _, cat := range cats {
 		fmt.Println(cat)
 	}
-	dataChan := make(chan requests.DataRequest)
-	resultChan := make(chan requests.ResultRequest)
-	finished := make(chan struct{}) //end of operations
-	workerDone := make(chan struct{})
-
-	finalResults := make(chan *[]cat.Cat)
 
 }
